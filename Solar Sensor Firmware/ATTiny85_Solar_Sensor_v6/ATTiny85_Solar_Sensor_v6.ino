d9a934153a5674ac87a0374c4b950e3ac7f3085a
Matthew Little <matt@re-innovation.co.uk>
2021-07-04 17:56:09 +0100

    Updated PCB v0.1

---
+++ Solar Sensor Firmware/ATTiny85_Solar_Sensor_v6/ATTiny85_Solar_Sensor_v6.ino
@@ -0,0 +1,285 @@
+/*
+   Solar Sensor firmware for ATTin85
+   by:    Matt Little
+   date:  20/6/2021
+   w:     www.curiouselectric.co.uk
+   e:     hello@curiouselectric.co.uk
+
+   This ciode is for an ATTiny85 unit solar sensor unit.
+   More concstruction details are here:
+   https://github.com/curiouselectric/SolarSensor
+   and
+
+
+   This unit serives serial port requests at 9600 baud.
+   It measures irradiance from an I2C ADC (ADS1115)
+   It measure temperature via an I2C temperature sensor (LM75B)
+   There is a push switch and LED for human interfacing.
+
+   It can be set to either:
+   Respond to serial data requests or constantly (at 1s, 10s and 1min times) output data.
+   Serial data requests:
+
+   Request data
+   "aAARQ#"
+   will return: "aAAI****T***#" or "aAAFAIL#"
+   Where AA is the device ID
+
+  Change ID
+  "aAACHIDBB#"
+  Will change the ID from AA to BB
+  Will return "aBBOK#" if OK or "aAAFAIL#" if not changed
+
+
+
+   Data supplied is the average irradiance level and the average temperature of the sensor.
+
+   At other times it sleeps, to ensure low power consumption
+
+   Fuses required:
+   Extended:  0xFF
+   High:      0xD6
+   Low:       0x62
+
+
+   TO DO:
+   Sort out return data on serial command
+   Sort out Stored ID and changing ID
+
+
+*/
+#define F_CPU 8000000  // This is used by delay.h library
+#include <stdlib.h>
+#include <EEPROM.h>
+#include <Arduino.h>
+#include <avr/io.h>        // Adds useful constants
+#include <util/delay.h>    // Adds delay_ms and delay_us functions
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+// Routines to set and clear bits (used in the sleep code)
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+#include <SoftwareSerial.h>
+#include <TinyWireM.h>                  // I2C Master lib for ATTinys which use USI
+#include <Adafruit_ADS1X15.h>
+
+
+#define pinI2C_SDA    PB0  // convert to arduino digital pins
+#define pinSW1        PB1
+#define pinI2C_CLK    PB2
+#define pinRx         PB3  // wake on interrupt here...  
+#define pinTx         PB4
+
+SoftwareSerial mySerial(pinRx, pinTx); //rx, tx
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+//Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+
+
+// *********** This is for the serial data parsing ******************
+char inChar;         // incoming serial byte
+String str_buffer = "";         // a String to hold incoming data
+String outputString = "";         // a String to hold outgoing data
+
+// Variables for the Sleep/power down modes:
+volatile boolean f_wdt = 1;
+
+// Useful variables
+char deviceID[3]; // A buffer to hold the device ID
+
+// Varibales for writing to EEPROM
+int hiByte;      // These are used to store longer variables into EERPRPROM
+int loByte;
+long int number;
+
+void setup() {
+
+  mySerial.begin(9600);
+
+  delay(500);
+  // Read in the values from EEPROM
+  // Read the device ID
+  deviceID[0] = char(EEPROM.read(0));
+  deviceID[1] = char(EEPROM.read(1));
+  mySerial.print("ID: ");
+  mySerial.print(deviceID[0]);
+  mySerial.println(deviceID[1]);
+
+  TinyWireM.begin();                    // initialize I2C lib
+
+  // ADC setup
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  ads.begin();
+
+  // Watch dog timer setup
+  setup_watchdog(8);      // approximately 4 seconds sleep
+
+  // Resevres some memory for the string buffer
+  str_buffer.reserve(50);
+  outputString.reserve(50);
+}
+
+void loop()
+{
+
+  // wait for timed out watchdog / flag is set when a watchdog timeout occurs
+  if (f_wdt == 1)
+  {
+    f_wdt = 0;     // reset flag
+    mySerial.print("Data: ");
+    // Test ADC
+    /* Be sure to update this value based on the IC and the gain settings! */
+    float multiplier = 0.125F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
+    int32_t dataValue = ads.readADC_Differential_2_3();
+    float dataFloat = (float)dataValue * (float)multiplier;
+    mySerial.println(dataFloat, 0);
+    //    // Service any Serial requests/data
+    //    mySerial.println("Read Data"); //going to sleep
+    system_sleep();  // Send the unit to sleep
+  }
+
+  if (mySerial.available())
+  {
+    if (getData())
+    {
+      // If the data is in an OK format (with a at start and # at end:
+      checkCommand();
+      mySerial.println(outputString);
+    }
+    else
+    {
+      mySerial.println("FAIL"); //going to sleep
+    }
+    mySerial.println(".zZ"); //going to sleep
+    mySerial.flush();
+    system_sleep();  // Send the unit to sleep
+  }
+}
+
+void system_sleep()
+{
+  cbi(ADCSRA, ADEN);                   // switch Analog to Digitalconverter OFF
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  sleep_mode();                        // System actually sleeps here
+  // ...zzzZZZzzz...
+  sleep_disable();                     // System continues execution here when watchdog timed out
+  sbi(ADCSRA, ADEN);                   // switch Analog to Digitalconverter ON
+  // Want to disable interrupts here:
+  GIMSK &= ~(_BV(INT0));  //disable INT0
+}
+
+//****************************************************************
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(int ii)
+{
+  byte bb;
+  int ww;
+  if (ii > 9 ) ii = 9;
+  bb = ii & 7;
+  if (ii > 7) bb |= (1 << 5);
+  bb |= (1 << WDCE);
+  ww = bb;
+  MCUSR &= ~(1 << WDRF);
+  // start timed sequence
+  WDTCR |= (1 << WDCE) | (1 << WDE);
+  // set new watchdog timeout value
+  WDTCR = bb;
+  WDTCR |= _BV(WDIE);
+}
+
+// Watchdog Interrupt Service is executed when watchdog timed out
+ISR(WDT_vect) {
+  f_wdt = 1; // set global flag
+}
+
+boolean getData()
+{
+  // **********GET DATA*******************************************
+  // We want to find the bit of interesting data in the serial data stream
+  str_buffer = "";    // Reset the buffer
+  delay(100);   // Wait for data to come in... MUST have this!
+  while (mySerial.available())
+  {
+    inChar = (char)mySerial.read(); // Read whatever is happening on the serial port
+    mySerial.print(inChar);
+    str_buffer += inChar;
+    if ((inChar == '#') || (inChar == '\r') || (inChar == '\n'))
+    {
+      break;
+    }
+  }
+  return(true);
+//  if (str_buffer[0] == 'a' && (str_buffer[str_buffer.length() - 1] == '#'))
+//  {
+//    return (true);
+//  }
+//  else
+//  {
+//    return (false);
+//  }
+}
+
+void checkCommand()
+{
+  mySerial.print("Checking data:");  // TEST - print the str_buffer data (if it has arrived)
+  mySerial.println(str_buffer);  // TEST - print the str_buffer data (if it has arrived)
+
+  outputString = "";
+  // If we get here then the start/end chars are correct:
+  if (str_buffer.length() > 50)
+  {
+    outputString = "Too Long";   // String is too long - should never happen...
+  }
+  else if (str_buffer.substring(3, 7) == "CHID")
+  {
+    // This does not care what the initial ID is
+    outputString = "CHID:";
+    outputString += deviceID[0];
+    outputString += deviceID[1];
+    deviceID[0] = str_buffer[7];
+    deviceID[1] = str_buffer[8];
+    // Also want to store this into EEPROM
+    EEPROM.write(0, deviceID[0]);    // Do this seperately
+    EEPROM.write(1, deviceID[1]);
+    outputString += "->";
+    outputString += deviceID[0];
+    outputString += deviceID[1];
+
+  }
+  else if (str_buffer.substring(3, 5) == "ID")
+  {
+    // This does not care what the initial ID is
+    outputString = "ID:";
+    outputString += deviceID[0];
+    outputString += deviceID[1];
+  }
+  else if (str_buffer.charAt(1) == deviceID[0] && str_buffer.charAt(2) == deviceID[1])
+  {
+    // This is where we need to parse the data
+    // Check the ID corresponds to the stored EEPROM ID
+    outputString = "ID OK";
+  }
+  else
+  {
+    outputString = "ID FAIL";
+  }
+}
--- Solar Sensor PCB/SolarSensorPCB.pro
+++ Solar Sensor PCB/SolarSensorPCB.pro
@@ -1,4 +1,4 @@
-update=20/06/2021 12:34:55
+update=04/07/2021 17:10:06
 version=1
 last_client=kicad
 [cvpcb]
@@ -36,6 +36,7 @@ MinMicroViaDiameter=0.2
 MinMicroViaDrill=0.09999999999999999
 MinHoleToHole=0.25
 TrackWidth1=0.5
+TrackWidth2=0.2
 ViaDiameter1=0.6
 ViaDrill1=0.4
 dPairWidth1=0.2
@@ -64,7 +65,7 @@ OthersTextUpright=1
 SolderMaskClearance=0.2
 SolderMaskMinWidth=0
 SolderPasteClearance=0
-SolderPasteRatio=0
+SolderPasteRatio=-0
 [pcbnew/Layer.F.Cu]
 Name=F.Cu
 Type=0
